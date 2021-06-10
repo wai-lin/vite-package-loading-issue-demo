986009a3299ac1b28fc3e624930f0faa200c138a
Wai Lin Aung <wailinaung@Wais-MacBook-Air-2.local>
2021-06-10 13:11:59 +0630

    chore(demo): init

---
+++ README.md
@@ -0,0 +1,7 @@
+# `twilio-video` cannot load in vite while in vue cli
+
+> Need npm version 7 for `workspaces`
+
+To run vite dev `npm run dev:vite`
+
+To run vue-cli dev `npm run dev:vue-cli`
---
+++ twilio-vue-cli/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Vue CLI</div>
+</template>
+
+<script>
+import { defineComponent, onMounted } from "vue";
+import { connect } from "twilio-video";
+
+export default defineComponent({
+  setup() {
+    onMounted(() => {
+      console.log(connect)
+    })
+  }
+})
+</script>
